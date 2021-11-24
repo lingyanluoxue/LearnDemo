lingyanluoxue/FeatureDemo

start activity for result

app/src/main/java/com/lylx/featuredemo/MainActivity.kt
@@ -1,0 +1,18 @@
+package com.lylx.featuredemo
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    findViewById<View>(R.id.btn_activity_result).setOnClickListener {
+      startActivity(Intent(this, ResultActivity::class.java))
+    }
+  }
+}

app/src/main/java/com/lylx/featuredemo/ResultActivity.kt
@@ -1,0 +1,82 @@
+package com.lylx.featuredemo
+
+import android.Manifest
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.lylx.featuredemo.provider.getUriForFile
+import com.lylx.featuredemo.util.createImageFile
+import kotlinx.android.synthetic.main.activity_result.*
+
+class ResultActivity : AppCompatActivity() {
+
+  companion object {
+    private const val TAG = "ResultActivity"
+    private const val MIME_IMAGE = "image/*"
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_result)
+
+    selectPhoto.setOnClickListener {
+      photoLauncher.launch(MIME_IMAGE)
+    }
+
+    takePicture.setOnClickListener {
+//      Environment.getExternalStorageDirectory()?.resolve("${Environment.DIRECTORY_PICTURES}/image.png")?.let {
+//        takePictureLauncher.launch(getUriForFile(this, it))
+//      }
+      createImageFile(this)?.let {
+        takePictureLauncher.launch(getUriForFile(this, it))
+      }
+    }
+
+    requestPermission.setOnClickListener {
+      requestPermissionLauncher.launch(Manifest.permission.READ_PHONE_STATE)
+    }
+
+    requestMultiplePermissions.setOnClickListener {
+      requestMultiplePermissionsLauncher.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE))
+    }
+  }
+
+  /**
+   * GetContent : 通过 Intent.ACTION_GET_CONTENT 获取一个文件
+   * @param : MIME 类型
+   * @return : 文件 Uri
+   */
+  private val photoLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+    Log.d(TAG, "select photo uri=$it")
+  }
+
+  /**
+   * TakePicture : 通过 MediaStore.ACTION_IMAGE_CAPTURE 拍照并保存
+   * @param : 保存文件的 Uri
+   * @return : 是否保存成功
+   */
+  private val takePictureLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) {
+      Log.d(TAG, "take picture result : $it")
+  }
+
+  /**
+   * TakePicture : 请求单个权限
+   * @param : Manifest.permission.*
+   * @return : 用户是否授予权限
+   */
+  private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+    Log.d(TAG, "permission is granted : $it")
+  }
+
+  /**
+   * TakePicture : 请求多个权限
+   * @param : Array<Manifest.permission.*>
+   * @return : Map<Manifest.permission.*, 用户是否授予权限>
+   */
+  private val requestMultiplePermissionsLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+    it.map { permissionResult ->
+      Log.d(TAG, "${permissionResult.key} is granted : ${permissionResult.value}")
+    }
+  }
+}

app/src/main/java/com/lylx/featuredemo/provider/GenericFileProvider.kt
@@ -1,0 +1,17 @@
+package com.lylx.featuredemo.provider
+
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import androidx.core.content.FileProvider
+import java.io.File
+
+private const val authority = ".fileprovider"
+
+fun getUriForFile(context: Context, file: File): Uri? {
+  return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+    FileProvider.getUriForFile(context, context.packageName.plus(authority), file)
+  } else {
+    Uri.fromFile(file)
+  }
+}

app/src/main/java/com/lylx/featuredemo/util/FileUtil.kt
@@ -1,0 +1,23 @@
+package com.lylx.featuredemo.util
+
+import android.app.Activity
+import android.os.Environment
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun createImageFile(activity: Activity, prefix: String? = "", suffix: String? = ""): File? {
+    val file: File?
+    val timeStamp: String = SimpleDateFormat("yyyy_MM_dd_HHmmss", Locale.US).format(Date())
+    val storageDir: File? = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    try {
+        file = File.createTempFile(
+            prefix?.plus(timeStamp) ?: timeStamp,
+            suffix?.plus(".png"),
+            storageDir
+        )
+    } finally {
+
+    }
+    return file
+}

lingyanluoxue/LearnDemo

app/src/main/java/com/lylx/learnfeature/MainActivity.kt
@@ -1,0 +1,18 @@
+package com.lylx.learnfeature
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    findViewById<View>(R.id.btn_activity_result).setOnClickListener {
+      startActivity(Intent(this, ResultActivity::class.java))
+    }
+  }
+}

app/src/main/java/com/lylx/learnfeature/ResultActivity.kt
@@ -1,0 +1,84 @@
+package com.lylx.learnfeature
+
+import android.Manifest
+import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.lylx.learnfeature.provider.getUriForFile
+import com.lylx.learnfeature.util.createImageFile
+import kotlinx.android.synthetic.main.activity_result.*
+
+class ResultActivity : AppCompatActivity() {
+
+  companion object {
+    private const val TAG = "ResultActivity"
+    private const val MIME_IMAGE = "image/*"
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_result)
+
+    selectPhoto.setOnClickListener {
+      photoLauncher.launch(MIME_IMAGE)
+    }
+
+    takePicture.setOnClickListener {
+//      Environment.getExternalStorageDirectory()?.resolve("${Environment.DIRECTORY_PICTURES}/image.png")?.let {
+//        takePictureLauncher.launch(getUriForFile(this, it))
+//      }
+      createImageFile(this)?.let {
+        takePictureLauncher.launch(getUriForFile(this, it))
+      }
+    }
+
+    requestPermission.setOnClickListener {
+      requestPermissionLauncher.launch(Manifest.permission.READ_PHONE_STATE)
+    }
+
+    requestMultiplePermissions.setOnClickListener {
+      requestMultiplePermissionsLauncher.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE))
+    }
+  }
+
+  /**
+   * GetContent : 通过 Intent.ACTION_GET_CONTENT 获取一个文件
+   * @param : MIME 类型
+   * @return : 文件 Uri
+   */
+  private val photoLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+    Log.d(TAG, "select photo uri=$it")
+  }
+
+  /**
+   * TakePicture : 通过 MediaStore.ACTION_IMAGE_CAPTURE 拍照并保存
+   * @param : 保存文件的 Uri
+   * @return : 是否保存成功
+   */
+  private val takePictureLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()) {
+      Log.d(TAG, "take picture result : $it")
+  }
+
+  /**
+   * TakePicture : 请求单个权限
+   * @param : Manifest.permission.*
+   * @return : 用户是否授予权限
+   */
+  private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+    Log.d(TAG, "permission is granted : $it")
+  }
+
+  /**
+   * TakePicture : 请求多个权限
+   * @param : Array<Manifest.permission.*>
+   * @return : Map<Manifest.permission.*, 用户是否授予权限>
+   */
+  private val requestMultiplePermissionsLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+    it.map { permissionResult ->
+      Log.d(TAG, "${permissionResult.key} is granted : ${permissionResult.value}")
+    }
+  }
+}

app/src/main/java/com/lylx/learnfeature/provider/GenericFileProvider.kt
@@ -1,0 +1,17 @@
+package com.lylx.learnfeature.provider
+
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import androidx.core.content.FileProvider
+import java.io.File
+
+private const val authority = ".fileprovider"
+
+fun getUriForFile(context: Context, file: File): Uri? {
+  return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+    FileProvider.getUriForFile(context, context.packageName.plus(authority), file)
+  } else {
+    Uri.fromFile(file)
+  }
+}

app/src/main/java/com/lylx/learnfeature/util/FileUtil.kt
@@ -1,0 +1,23 @@
+package com.lylx.learnfeature.util
+
+import android.app.Activity
+import android.os.Environment
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun createImageFile(activity: Activity, prefix: String? = "", suffix: String? = ""): File? {
+    val file: File?
+    val timeStamp: String = SimpleDateFormat("yyyy_MM_dd_HHmmss", Locale.US).format(Date())
+    val storageDir: File? = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    try {
+        file = File.createTempFile(
+            prefix?.plus(timeStamp) ?: timeStamp,
+            suffix?.plus(".png"),
+            storageDir
+        )
+    } finally {
+
+    }
+    return file
+}