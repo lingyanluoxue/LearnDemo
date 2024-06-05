fab195565db1f7c08c3a816e90ddfe4ee21ab02f
May <zhanghongmei@dayuwuxian.com>
2024-06-05 10:37:19 +0800

    jsoup

--- app/src/main/java/com/lylx/learnfeature/MainActivity.kt
+++ app/src/main/java/com/lylx/learnfeature/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.lylx.learnfeature
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.jsoup.Jsoup
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +24,25 @@ class MainActivity : AppCompatActivity() {
 
     findViewById<View>(R.id.btn_notification).setOnClickListener {
       startActivity(Intent(this, NotificationActivity::class.java))
+    }
+
+    findViewById<View>(R.id.btn_jsoup).setOnClickListener {
+      lifecycleScope.launch {
+        val lyricUrl = Uri
+          .parse("https://www.google.com/search")
+          .buildUpon()
+          .appendQueryParameter("q", "lyric 李志 - 关于郑州的记忆").toString()
+        val result = withContext(Dispatchers.IO) {
+          val document = Jsoup.connect(lyricUrl).timeout(5000).get()
+          val search = document?.getElementsByClass("PZPZlf")?.firstOrNull {
+            it.className() == "PZPZlf"
+          }
+          search?.select("span")?.mapNotNull {
+            it.text()
+          }
+        }
+        Log.d("lylx", "result:$result")
+      }
     }
   }
 
